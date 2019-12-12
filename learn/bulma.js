0e533fb409f2e03a3aba9fafe9b6db3f8099974f
shadabshaikh0 <sshadabshaikh7703@gmail.com>
2019-12-12 18:52:31 +0530

    UI for logged in user started Bulma 2

--- learn/bulma.js
+++ learn/bulma.js
@@ -1,4 +1,19 @@
 
+function ready(){
+  // If is part of some team
+  in_team = false
+  if(in_team){
+    document.getElementById('create_join_section').style.display = "none" ; 
+    document.getElementById('team_members_list_section').style.display = "block" ; 
+  }
+  else{
+    document.getElementById('team_members_list_section').style.display = "none" ; 
+    document.getElementById('create_join_section').style.display = "block" ; 
+  }
+
+  hideAll();
+}
+
 function hideAll() { 
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
