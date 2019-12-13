c939351a5c6229165083e9678a7a877582446a9f
shadabshaikh0 <sshadabshaikh7703@gmail.com>
2019-12-14 00:47:43 +0530

    --final: before jwt token merging

--- app.js
+++ app.js
@@ -75,21 +75,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
-app.use((req, res, next) => {
-  if (req.path === '/api/upload') {
-    // Multer multipart/form-data handling needs to occur before the Lusca CSRF check.
-    next();
-  } else {
-    lusca.csrf()(req, res, next);
-  }
-});
-app.use(lusca.xframe('SAMEORIGIN'));
-app.use(lusca.xssProtection(true));
-app.disable('x-powered-by');
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
-});
+// app.use((req, res, next) => {
+//   if (req.path === '/api/upload') {
+//     // Multer multipart/form-data handling needs to occur before the Lusca CSRF check.
+//     next();
+//   } else {
+//     lusca.csrf()(req, res, next);
+//   }
+// });
+// app.use(lusca.xframe('SAMEORIGIN'));
+// app.use(lusca.xssProtection(true));
+// app.disable('x-powered-by');
+// app.use((req, res, next) => {
+//   res.locals.user = req.user;
+//   next();
+// });
 app.use((req, res, next) => {
   // After successful login, redirect back to the intended page
   if (!req.user
@@ -114,7 +114,12 @@ app.get('/', homeController.index);
 app.get('/login', homeController.login);
 app.get('/profile', homeController.profile);
 app.get('/signup', homeController.signup);
+app.post('/api/foo', (req,res)=>{
+    id = req.body.id ;    
 
+    
+    res.json({ "data":"bar"})
+})
 
 
 // app.post('/login', userController.postLogin);
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,66 @@
+
+function display_dashboard( payload  ){
+    // let userdata = payload.userdata;
+    // let teamdata = payload.teamdata;
+
+    userdata = {
+      gravatar : 'https://bulma.io/images/placeholders/128x128.png',
+      name : 'Shadab Majid Shaikh',
+      _id : 'C39824782738',
+      email : 'shadabshaik@gamil.com',
+      mobile : '8974573495',
+      dept : 'Computer',
+      year : 'BE'
+    }
+
+
+    document.getElementById('user_gravatar').src = userdata.gravatar ; 
+    document.getElementById('user_name').innerText = userdata.name ; 
+    document.getElementById('user_regid').innerText = userdata._id ; 
+    document.getElementById('user_email').innerText = userdata.email ; 
+    document.getElementById('user_deptyear').innerText = userdata.dept + ', ' + userdata.year ; 
+    document.getElementById('user_mobile').innerText = userdata.mobile ; 
+
+    teamdata = {
+      teamname    : "Simplifiers",
+      teamid      : "1",
+      membername1 : "Shadab Shaikh",
+      membername2 : "Surya Jha",
+      membername3 : "Sairaj Sawant",
+      membername4 : "Shubham Rekkawar",
+      membername5 : "Priyanka Khot",
+      membername6 : "Pallavi Paliwal",
+    }
+
+    document.getElementById('teamname').innerText    = teamdata.teamname ; 
+    document.getElementById('teamid').innerText      = teamdata.teamid ; 
+    document.getElementById('membername1').innerText = teamdata.membername1 ; 
+    document.getElementById('membername2').innerText = teamdata.membername2 ; 
+    document.getElementById('membername3').innerText = teamdata.membername3 ; 
+    document.getElementById('membername4').innerText = teamdata.membername4 ; 
+    document.getElementById('membername5').innerText = teamdata.membername5 ; 
+    document.getElementById('membername6').innerText = teamdata.membername6 ; 
+
+
+  }
+
+function loaddata() {
+  data = { id: 'C2K17207238' };
+  fetch('http://localhost:8000/api/foo', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(function (res) {
+      console.log(res);
+      display_dashboard(res)
+
+    });
+}
+
 function ready() {
   // If is part of some team
   in_team = true
@@ -9,6 +72,7 @@ function ready() {
     document.getElementById('create_join_section').style.display = "block";
   }
   hideAll();
+  loaddata();
 }
 
 function hideAll() {
