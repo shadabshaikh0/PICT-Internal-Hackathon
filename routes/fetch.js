7ea90d27c61a790a768be61a03f51c1f7812ce09
shadabshaikh0 <sshadabshaikh7703@gmail.com>
2019-12-14 09:53:46 +0530

    --fix: dynamic profile loading iron man

---
+++ routes/fetch.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const fetchController = require("../controllers/fetch");
+
+const router = express.Router();
+
+router
+	.route('/user_dashboard')
+    .post(fetchController.load_user_dashboard)
+    
+
+module.exports = router;
