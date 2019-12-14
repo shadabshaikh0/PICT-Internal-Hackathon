7c9ca6ce1f64955226b99ecfd9c54d9728c95243
Suryaa Jha <suryaa.jha.ms@gmail.com>
2019-12-14 16:05:24 +0530

    Local Changes

--- learn/bulma.js
+++ learn/bulma.js
@@ -1,7 +1,7 @@
 
 function ready(){
   // If is part of some team
-  in_team = false
+  in_team = true
   if(in_team){
     document.getElementById('create_join_section').style.display = "none" ; 
     document.getElementById('team_members_list_section').style.display = "block" ; 
