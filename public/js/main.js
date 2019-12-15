999e140cc0dd6e895f9c2e0376cb3e5a65281468
Suryaa Jha <suryaa.jha.ms@gmail.com>
2019-12-16 02:44:33 +0530

    Added Update route with implementation

--- public/js/main.js
+++ public/js/main.js
@@ -99,6 +99,9 @@ function display_dashboard(payload) {
   console.log(payload);
 
   let userdata = payload.userdata;
+
+  // Save for later retrieval
+  localStorage.setItem('user_info', JSON.stringify(userdata));
   // let teamdata = payload.teamdata;
 
   // userdata = {
