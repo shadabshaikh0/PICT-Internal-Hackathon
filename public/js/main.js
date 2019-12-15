4ec948371841ee6e2b1e6f661f06175de919d180
Sairaj Sawant <sairajsawant30@gmail.com>
2019-12-15 23:26:58 +0530

    --add: bot support on home & login

--- public/js/main.js
+++ public/js/main.js
@@ -184,9 +184,20 @@ function loaddata() {
 }
 
 function ready() {
+  loadBot();
   hideAll();
   loaddata();
   tabLoad() ; 
+}
+
+function loadBot() {
+  (function(d, m){
+    var kommunicateSettings = {"appId":"28703ea1cf3fa6f7bf9c8402651c03d74","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {}); 
 }
 
 function getGetParam(param){
