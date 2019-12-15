9af2850e7dc3b94d9492c1ac4232574f0a3fd77a
Pallavi Paliwal <pallavipaliwal1999@gmail.com>
2019-12-15 22:11:25 +0530

    Updating PR

--- controllers/main.js
+++ controllers/main.js
@@ -1,8 +1,4 @@
 let landingPage = function (req, res) {
-<<<<<<< HEAD
-	console.log("index page");
-=======
->>>>>>> dbafc05fc7db22d1e05370714d3237e028fc0e58
 	res.sendFile('public/index.html');
 }
 
